d4983a69d6c2439f0e09668edbf0909b73b5bad1
Alex Lindemann <AlexDeadmanLindemann@gmail.com>
2022-08-16 14:33:53 -0500

    Testing

--- src/App.js
+++ src/App.js
@@ -8,15 +8,14 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          App TWO
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://test-installable-one.herokuapp.com/"
           target="_blank"
-          rel="noopener noreferrer"
         >
-          Learn React
+          GO TO APP ONE
         </a>
       </header>
     </div>
