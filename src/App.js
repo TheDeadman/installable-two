bbd938aaa94d53403c5bcd2d1c63350365f5d2b7
Alex Lindemann <AlexDeadmanLindemann@gmail.com>
2022-08-16 14:47:10 -0500

    Testing

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
-import React from 'react';
+import { useState } from 'react'
+import { useLocation } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [hashTarget, setHashTarget] = useState('')
+  const location = useLocation();
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          App TWO
+          App TWO - {location.hash}
         </p>
+        <input type="text" value={hashTarget} onChange={(e) => setHashTarget(e.target.value)} />
         <a
           className="App-link"
-          href="https://test-installable-one.herokuapp.com/"
+          href={`https://test-installable-two.herokuapp.com/${hashTarget ? "#" + hashTarget : ''}`}
           target="_blank"
         >
           GO TO APP ONE
