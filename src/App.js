142c727ed6ca186fe2b95c1c6b2224c9510dc6b2
Alex Lindemann <AlexDeadmanLindemann@gmail.com>
2022-08-16 15:24:37 -0500

    Testing

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { useLocation } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [now, setNow] = useState(0)
   const [hashTarget, setHashTarget] = useState('')
   const location = useLocation();
+  useEffect(() => {
+    setNow(Date.now());
+  }, [])
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          App TWO - {location.hash}
+          App TWO - {location.hash} - {now}
         </p>
         <input type="text" value={hashTarget} onChange={(e) => setHashTarget(e.target.value)} />
         <a
